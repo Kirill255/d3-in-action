3dcbec384c7eb1cb58f6c2e9eced25bf7c9141bf
Kirill255 <24504648+Kirill255@users.noreply.github.com>
2019-05-26 12:05:25 +0300

    start project

---
+++ graph.js
@@ -0,0 +1,36 @@
+const dims = { height: 300, width: 300, radius: 150 };
+const cent = { x: dims.width / 2 + 5, y: dims.height / 2 + 5 };
+
+// create svg container
+const svg = d3
+  .select(".canvas")
+  .append("svg")
+  .attr("width", dims.width + 150)
+  .attr("height", dims.height + 150);
+
+const graph = svg.append("g").attr("transform", `translate(${cent.x}, ${cent.y})`); // translates the graph group to the middle of the svg container
+
+const pie = d3
+  .pie()
+  .sort(null)
+  .value((d) => d.cost); // the value we are evaluating to create the pie angles
+
+const angles = pie([
+  { name: "rent", cost: 500 },
+  { name: "bills", cost: 300 },
+  { name: "gaming", cost: 200 }
+]);
+
+console.log(angles);
+
+const arcPath = d3
+  .arc()
+  .outerRadius(dims.radius)
+  .innerRadius(dims.radius / 2);
+
+console.log(arcPath(angles[0]));
+
+graph
+  .append("path")
+  .attr("d", arcPath(angles[0]))
+  .attr("fill", "orange");
